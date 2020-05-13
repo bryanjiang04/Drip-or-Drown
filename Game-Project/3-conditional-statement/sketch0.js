a6dfd9035444482789c3e40d24f7a9b7a4b16dec
bryanjiang04 <bryanjiang04@gmail.com>
2020-05-13 12:20:29 -0700

    Create sketch0.js

---
+++ Game-Project/3-conditional-statement/sketch0.js
@@ -0,0 +1,28 @@
+// Creating animations
+//
+// animations like p5 images should be stored in variables
+// in order to be displayed during the draw cycle
+var sprites;
+//
+// //it's advisable (but not necessary) to load the images in the preload function
+// //of your sketch otherwise they may appear with a little delay
+function preload() {
+//
+//   //create an animation from a sequence of numbered images
+//   //pass the first and the last file name and it will try to find the ones in between
+  sprites = loadAnimation('sprites/goldstar.png');
+
+}
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(0);
+
+  //specify the animation instance and its x,y position
+  //animation() will update the animation frame as well
+  animation(sprites, 500,500);
+
+}
